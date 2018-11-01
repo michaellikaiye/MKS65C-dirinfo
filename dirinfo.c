3414b8b2090f0a4a6e72db21913187c0eccde7d1
michaellikaiye <mye1@stuy.edu>
2018-11-01 15:28:15 -0400

    k

---
+++ dirinfo.c
@@ -0,0 +1,29 @@
+
+/* list all the files in the directory */
+/* specify which files are directories (if any) */
+/* show the total size of all the regular files the directory */
+/* note that you do not have to recursively go through any subdirectories to find their size for this part (unless you want to, but that is not a simple task) */
+/* Have the program scan the current directory */
+/* Possible enhancements: */
+/* Recursively list the files in any subdirectories, update the total to include the total size of subdirectories */
+/* Print out the size in a more readable format (like using KB, MB, GB for -byte prefixes) */
+
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<dirent.h>
+#include<sys/types.h>
+
+int main() {
+  DIR * d;
+  d = opendir("testing");
+  struct dirent *entry;
+  entry = readdir (d);
+  closedir(d);
+
+  return 0;
+}
