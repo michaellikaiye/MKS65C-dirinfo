cddc13d670b74ccfec3efe6b473d2627b0a43470
Karen Li <kli19@stuy.edu>
2018-11-03 21:07:22 -0400

    formatting, comments, found a bug

--- dirinfo.c
+++ dirinfo.c
@@ -34,6 +34,7 @@ void printPermissions(int fileMode) {
   printHelper(fileMode, S_IXOTH, S_IEXEC, 'x');
 }
 
+// prints file size in readable format
 void printSize(double size) {
   char * sizearray[5] = {"", "K", "M", "G", "T"};
   int ps = 0;
@@ -44,29 +45,44 @@ void printSize(double size) {
   printf("%f %sB", size, sizearray[ps]);
 }
 
-
+// prints file info
 void printStat(char * str) {
-  struct stat * pter = malloc(sizeof(struct stat)); // allocate some memory
-  stat(str, pter); // record metadata into pter
-  // printing file info:
+  // allocate some memory
+  struct stat * pter = malloc(sizeof(struct stat));
+
+  // record metadata into pter
+  stat(str, pter);
   printf("file: %s\n", str);
+
+  // prints file size
   long int size = pter -> st_size;
   printSize(size);
+
+  //prints file permissions
   printf("mode(permissions): (%3o)\n", pter -> st_mode&0777);
   printPermissions(pter -> st_mode);
+
+  //prints time of last access
   char temp[100];
   strftime(temp, 100, "%c", localtime(&(pter -> st_atime)));
   printf("\ntime accessed: %s\n", temp);
+
+  // frees memory
   free(pter);
 }
 
+// prints directory information (tab is for formatting based on the directory level)
 long int printDir(char *str, int tab) {
+  
+  // for indentation when printing
   char tabString[100];
   int i;
   for(i = 0; i < 3 * tab; i++) {
     tabString[i] = ' ';
   }
   tabString[i] = '\0';
+
+  // opens directory stream
   struct dirent *entry;
   long int fsize = 0;
   DIR * d;
@@ -77,45 +93,79 @@ long int printDir(char *str, int tab) {
     printf("%sCould not open current directory\n", tabString);
     return 0;
   }
+
+  // prints directory information
   while(entry = readdir(d)){
+
+    // allocate some memory for file information
     struct stat * po = malloc(sizeof(struct stat));
     stat(entry->d_name, po);
+
+    // prints information in the current directory but prevents recursively printing the directory itself
     if (entry->d_type == 4 && strncmp(entry->d_name, ".", 1) != 0) {
-      printf("%s%s (directory name)\n", tabString, entry->d_name);   
+
+      // prints directory name
+      printf("%sDirectory name: %s\n", tabString, entry->d_name);   
       printf("%s  ", tabString);
+
+      // prints directory permissions
+      printf(" Directory permissions:");
       printPermissions(po->st_mode);
-      printf(" (directory permissions)\n");
-      printf("%s  ", tabString);
+      printf("\n%s  ", tabString);
+
+      // prints directory size
+      printf(" Directory size: ");
       printSize(po->st_size);
-      printf(" (directory size)\n");
+      printf("\n\n");
+
+      
       printDir(entry->d_name, tab + 3);
     }
 
     else if (entry->d_type == 8){
-      
       stat(entry->d_name, po);
-      printf("%s%s (file name)\n", tabString, entry->d_name);
+
+      // prints file name
+      printf("%sFile name: %s\n", tabString, entry->d_name);
       printf("%s  ", tabString);
+
+      // prints file permissions
+      // ==============ERROR HERE: ST_MODE RETURNS 0=============
+      printf("File permissions: ");
       printPermissions(po->st_mode);
-      printf(" (file permissions)\n");
-      printf("%s  ", tabString);
+      printf("\n %s  ", tabString);
+
+      // prints file size
+      printf("file size: ");
       printSize(po->st_size);
-      printf(" (file size)\n");
+      printf("\n\n");
+
+
+      // updates total size
       fsize += po->st_size;
+
+      //frees memory
       free(po);
     }
     else;
   }
+
+  // prints total directory size
   printf("%sdirectory %s total size:", tabString, str);
   printSize(fsize);
   printf("\n");
+
+  // closes directory
   closedir(d);
+  
   return fsize;
 }
 
+
+
 int main() {
   int totalSize;
-  printf("stuff in current dirctory: \n");
+  printf("current directory information: \n");
   totalSize = printDir(".", 1);
   //printStat("dirinfo.c");
 
