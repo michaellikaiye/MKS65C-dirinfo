42ca619b141865201153b67dffc14aa12a050b4e
michaellikaiye <mye1@stuy.edu>
2018-11-04 13:22:21 -0500

    extra

--- dirinfo.c
+++ dirinfo.c
@@ -34,15 +34,23 @@ void printPermissions(int fileMode) {
   printHelper(fileMode, S_IXOTH, S_IEXEC, 'x');
 }
 
+static int myCompare (const void * a, const void * b) {
+  return strcmp (*(const char **) a, *(const char **) b);
+}
+
+void sort(const char *arr[], int n) {
+  qsort (arr, n, sizeof (const char *), myCompare);
+}
+
 // prints file size in readable format
 void printSize(double size) {
-  char * sizearray[5] = {"", "K", "M", "G", "T"};
+  char * sizearray[5] = {" ", "K", "M", "G", "T"};
   int ps = 0;
   while(size > 1000) {
     size = size / 1000.0;
     ps++;
   }
-  printf("%f %sB", size, sizearray[ps]);
+  printf("%6.2f %sB", size, sizearray[ps]);
 }
 
 // prints file info
@@ -55,25 +63,25 @@ void printStat(char * str) {
 
   //prints file permissions
   //printf("mode(permissions): (%3o)\n", pter -> st_mode&0777);
-  printf("-");
   printPermissions(pter -> st_mode);
-  printf("  ");
+  printf(" ");
 
   //print user id and group id of owner
   struct passwd * pwd =  malloc(sizeof(struct passwd));
   pwd = getpwuid(pter->st_uid);
-  printf("  %s  ", pwd->pw_name);
+  printf("  %s", pwd->pw_name);
   pwd = getpwuid(pter->st_gid);
-  printf("  %s  ", pwd->pw_name);
-  
-  //prints time of last access
-  char temp[100];
-  strftime(temp, 100, "%c", localtime(&(pter -> st_atime)));
-  printf(" %s  ", temp);
+  printf(" %s ", pwd->pw_name);
 
   // prints file size
   long int size = pter -> st_size;
   printSize(size);
+
+  //prints time of last access
+  char temp[100];
+  strftime(temp, 100, "%b %e %H:%M", localtime(&(pter -> st_atime)));
+  printf(" %s  ", temp);
+
 
   //prints file name
   printf(" %s\n", str);
@@ -84,19 +92,37 @@ void printStat(char * str) {
 
 // prints directory information (tab is for formatting based on the directory level)
 long int printDir(char *str) {
-  
+  printf("\n%s directory information: \n", str);
   // opens directory stream
   struct dirent *entry;
   long int fsize = 0;
   DIR * d;
   d = opendir(str);
+  if (d == NULL) {
+    printf("Could not open current directory 1\n");
+    return 0;
+  }
+  int dirNum = 0;
+  int filNum = 0;
+  while(entry = readdir(d)){
+    if (entry->d_type == 4 && strncmp(entry->d_name, ".", 1) != 0)
+      dirNum++;
+    if (entry->d_type == 8)
+      filNum++;
+  }
+  printf("directories: %d\n", dirNum);
+  printf("files: %d\n",filNum);
+  closedir(d);
 
+  d = opendir(str);
   //error stuff?? how to implement
   if (d == NULL){
-    printf("Could not open current directory\n");
+    printf("Could not open current directory 2\n");
     return 0;
   }
 
+  char *dirArray[dirNum];
+  int dA = 0;
   // prints directory information
   while(entry = readdir(d)){
 
@@ -105,63 +131,53 @@ long int printDir(char *str) {
     stat(entry->d_name, po);
 
     // prints information in the current directory but prevents recursively printing the directory itself
-    if (entry->d_type == 4 && strncmp(entry->d_name, ".", 1) != 0) {
-
-
-      // prints directory permissions
+    if (entry->d_type == 4) {
+      printf("\033[0;34m");
       printf("d");
-      printPermissions(po->st_mode);
-      printf("  ");
-
-
-      // USER, GROUP, AND TIME ARE NOT AVAILABLE
-      printf(" USERID   GROUPID  ");
-      printf(" day mon dd hh:mm:ss yyyy  ");
-
-      // prints directory size
-      printSize(po->st_size);
-
-      // prints directory name
-      printf("  %s\n", entry->d_name);   
-
+      printStat(entry->d_name);
+      printf("\033[0m");
+      if(strncmp(entry->d_name, ".", 1) != 0) {
+        dirArray[dA] = entry->d_name;
+        dA++;
+      }
       //BROKEN
       //printDir(entry->d_name);
     }
 
     else if (entry->d_type == 8){
+      printf("-");
       stat(entry->d_name, po);
-
       printStat(entry->d_name);
-
       // updates total size
       fsize += po->st_size;
-
-      //frees memory
-      free(po);
     }
     else;
+    //frees memory
+    free(po);
   }
-
-  // prints total directory size
   printf("total size: ");
   printSize(fsize);
   printf("\n");
 
   // closes directory
   closedir(d);
-  
+
+  //print other subdirectories
+  //sort(dirArray, dA);
+  int h;
+  for(h = 0; h < dA; h++) {
+    printDir(dirArray[h]);
+  }
+  // prints total directory size
+
   return fsize;
 }
 
 
 
 int main() {
-  int totalSize;
-  printf("current directory information: \n");
-  totalSize = printDir(".");
-  //printStat("dirinfo.c");
-
-
+  printf("directory information (note that . files are ignored when printing subdirectories)\n\n");
+  printDir(".");
 
   return 0;
   }
